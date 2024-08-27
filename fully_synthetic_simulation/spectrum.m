4f6d48f39c49c0cba513567c614cb08fc94ac0db
Cestlab_Zu <126023075+CESTlabZu@users.noreply.github.com>
2024-08-27 15:26:00 -0500

    Add files via upload

---
+++ fully_synthetic_simulation/spectrum.m
@@ -0,0 +1,8 @@
+function y = spectrum(amp, width, off)
+    max=2000;
+    step=50;
+    delB0=0.5;
+    offset= -max:step:max;
+    rffreq=[-4000, -3500, -3000, -2500, offset, 2500, 3000,3500,4000]*delB0;
+    y = amp./(1+((((rffreq-off)*2*pi).^2)./((0.5*(abs(width))).^2)));
+end
