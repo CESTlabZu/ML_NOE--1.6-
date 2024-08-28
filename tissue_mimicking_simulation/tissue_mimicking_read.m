85215bc67029527159961f45c42153a0fa767128
Cestlab_Zu <126023075+CESTlabZu@users.noreply.github.com>
2024-08-28 14:43:49 -0500

    Update tissue_mimicking_read.m

--- tissue_mimicking_simulation/tissue_mimicking_read.m
+++ tissue_mimicking_simulation/tissue_mimicking_read.m
@@ -76,19 +76,19 @@ num_sep4=1;
 T1W_matrix=[1.4, 1.7, 2.0];
 T2W_matrix=[60, 90, 120]*0.001;
 T2S4_matrix=[0.0015, 0.0025, 0.0035];
-T2M_matrix = [50, 60, 70]*0.000001;
+T2M_matrix=[50, 60, 70]*0.000001;
 
 fs1_matrix=[0.6 1.0 1.4]*0.001;
 fs2_matrix=[0.6 1.0 1.4]*0.003;
-fs3_matrix = [0.0001, 0.0003, 0.0005];
+fs3_matrix=[0.0001, 0.0003, 0.0005];
 fs4_matrix=[0.001,0.0025,0.005];
 fs5_matrix=[0.6 1.0 1.4]*0.01;
 fm_matrix=[0.6 1.0 1.4]*0.07;
 
-ksw2_matrix = [3000, 5000, 7000];
+ksw2_matrix=[3000, 5000, 7000];
 
 % varied offset(B0 shift)
-off_var = [-30,0,30];
+off_var=[-30,0,30];
 
 
 
@@ -98,14 +98,14 @@ off_var = [-30,0,30];
 
 
 
-i=1;
+i = 1;
 
 for ii_T1W=1:num_T1W
     R1W=1./T1W_matrix(ii_T1W);
     for ii_T2W=1:num_T2W
         R2W=1./T2W_matrix(ii_T2W);
         for ii_T2M = 1:num_T2M
-            R2M = 1./T2M_matrix(ii_T2M);
+            R2M=1./T2M_matrix(ii_T2M);
             for ii_fs2=1:num_fs2
                 fs2=fs2_matrix(ii_fs2);
                 for ii_fs3=1:num_fs3
@@ -117,9 +117,9 @@ for ii_T1W=1:num_T1W
                             for ii_ksw2=1:num_ksw2
                                 ksw2=ksw2_matrix(ii_ksw2);
                                 for ii_offvar=1:num_offvar
-                                    var = off_var(ii_offvar);
+                                    var=off_var(ii_offvar);
                                         for ii_T2S4=1:num_T2S4
-                                            R2S4= 1./T2S4_matrix(ii_T2S4);
+                                            R2S4=1./T2S4_matrix(ii_T2S4);
                                             for ii_fs4=1:num_fs4
                                                 fs4=fs4_matrix(ii_fs4);
                                                 
@@ -130,7 +130,7 @@ k_4p7T=k_0+var;
 % apparent R1W
 R1W_cal_obs=(R1W+(fm*R1M))./(1+fm); 
 
-a25mspulse = runsteadysimgauss(ksw1, ksw2, ksw3, ksw4, ksw5, kmw, fs1, fs2, fs3,fs4, fs5, 1, fm, R1S, R2S1, R2S2, R2S3,R2S4, R2S5, R1W, R2W, R1M, R2M,sep1_4p7T*2*pi,sep2_4p7T*2*pi,sep3_4p7T*2*pi,sep4_4p7T*2*pi, sep5_4p7T*2*pi, pulseduration, gauss, satangle, 1, 2, 1, .00, 1, 1, k_4p7T*2*pi, 1);
+a25mspulse=runsteadysimgauss(ksw1, ksw2, ksw3, ksw4, ksw5, kmw, fs1, fs2, fs3,fs4, fs5, 1, fm, R1S, R2S1, R2S2, R2S3,R2S4, R2S5, R1W, R2W, R1M, R2M,sep1_4p7T*2*pi,sep2_4p7T*2*pi,sep3_4p7T*2*pi,sep4_4p7T*2*pi, sep5_4p7T*2*pi, pulseduration, gauss, satangle, 1, 2, 1, .00, 1, 1, k_4p7T*2*pi, 1);
 Zspectra_matrix(:,i)=a25mspulse(:,6);
 
 fm_matrix(i)=fm;
